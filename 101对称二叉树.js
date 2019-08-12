ecb75e4d332d466a342d190c9cb4d9a021e40dbf
1185491736@qq.com <1185491736@qq.com>
2019-08-12 11:42:32 +0800

    101对称二叉树

--- 101对称二叉树.js
+++ 101对称二叉树.js
@@ -10,5 +10,13 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    
+    if(!root) {
+        return true
+    }
+    let walk = (left,right) => {
+        if(!left && !right) return true
+        if((left&&!right) || (!left&&right) || (left.val!==right.val)) return false
+        return walk(left.left,right.right) && walk(left.right,right.left)
+    }
+    return walk(root.left,root.right)
 };
