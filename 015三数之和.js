54e65c9495ec4de393623c95fe16b8eca8dfb11b
1185491736@qq.com <1185491736@qq.com>
2019-08-24 21:08:29 +0800

    015

--- 015三数之和.js
+++ 015三数之和.js
@@ -1,7 +1,30 @@
 /**
- * @param {string} s
- * @return {number}
+ * @param {number[]} nums
+ * @return {number[][]}
  */
-var romanToInt = function(s) {
-    const a = [...new Set(s)];
+var threeSum = function(nums) {
+    let res = [];
+    let len = nums.length;
+    nums.sort((a,b) => a - b);
+    if(nums[0] <= 0 && nums[len-1] >= 0) {
+        for(let i = 0; i < len - 2;) {
+            if(nums[i] > 0) break;
+            let first = i + 1;
+            let last = len - 1;
+            do {
+                if(first > last || nums[i] * nums[last] > 0) break;
+                let result = nums[i] + nums[first] + nums[last];
+                if (result === 0) {
+                    res.push([nums[i],nums[first],nums[last]]);
+                }
+                if (result <= 0) {
+                    while (first<last && nums[first]===nums[++first]) {}
+                } else {
+                    while (first<last && nums[last]===nums[--last]) {}
+                }
+            } while(first < last)
+            while (nums[i] === nums[++i]) {}
+        }
+    }
+    return res;
 };
