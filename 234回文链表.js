1afe64ed7754690b94fdd86197269330ed3c1fad
1185491736@qq.com <1185491736@qq.com>
2019-08-29 19:29:31 +0800

    234

--- 234回文链表.js
+++ 234回文链表.js
@@ -19,3 +19,38 @@ var isPalindrome = function(head) {
     let b = map.reverse().join('');
     return a === b
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if (head === null || head.next === null) return true;
+    let mid = head;
+    let pre = null;
+    let reversed = null;
+    while (head !== null && head.next !== null) {
+        pre = mid;
+        // min慢指针，head快指针
+        mid = mid.next;
+        head = head.next.next;
+        pre.next = reversed;
+        reversed = pre;
+    }
+    if (head) mid = mid.next;
+    while (mid) {
+        if (reversed.val !== mid.val) return false;
+        reversed = reversed.next;
+        mid = mid.next;
+    }
+    return true
+};
