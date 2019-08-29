8838260af020cfbc50885b4ef8f2661bfcd7fe3b
1185491736@qq.com <1185491736@qq.com>
2019-08-29 23:08:29 +0800

    328

---
+++ 328奇偶链表.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if (head === null) {
+        return head;
+    }
+    let odd = head;
+    let even = odd.next;
+    let evenHead = even;
+    while (even !== null && even.next !== null) {
+        odd.next = odd.next.next;
+        odd = odd.next;
+        even.next = even.next.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head
+};
