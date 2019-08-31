cc933e95af9c865f039bae2c84fd3d9b494bd27d
1185491736@qq.com <1185491736@qq.com>
2019-08-31 17:41:10 +0800

    617

--- 617合并二叉树.js
+++ 617合并二叉树.js
@@ -14,7 +14,7 @@ var mergeTrees = function(t1, t2) {
     if (t1 === null && t2 === null) return null;
     if (t1 === null) return t2;
     if (t2 === null) return t1;
-    let root = new TreeNode(t1.val, t2.val);
+    let root = new TreeNode(t1.val + t2.val);
     root.left = mergeTrees(t1.left, t2.left);
     root.right = mergeTrees(t1.right, t2.right);
     return root;
