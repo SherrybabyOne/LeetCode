1788942c4f54a68ff9dc7bf15861d71dcedaa447
1185491736@qq.com <1185491736@qq.com>
2019-08-31 17:39:07 +0800

    617

---
+++ 617合并二叉树.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(t1, t2) {
+    if (t1 === null && t2 === null) return null;
+    if (t1 === null) return t2;
+    if (t2 === null) return t1;
+    let root = new TreeNode(t1.val, t2.val);
+    root.left = mergeTrees(t1.left, t2.left);
+    root.right = mergeTrees(t1.right, t2.right);
+    return root;
+};
