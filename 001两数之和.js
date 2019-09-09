1b405bf35d7569d5a2d143d316331e34c3f871fa
1185491736@qq.com <1185491736@qq.com>
2019-09-09 22:08:56 +0800

    001

--- 001两数之和.js
+++ 001两数之和.js
@@ -13,3 +13,16 @@ var twoSum = function(nums, target) {
         res[nums[i]] = i;
     }
 };
+
+
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            return [i, map.get(nums[i])]
+        } else {
+            map.set(target - nums[i], i)
+        }
+    }
+    return []
+};
