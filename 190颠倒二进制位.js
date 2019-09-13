b34a26a82828c365975b42eca2ae6c2bf3099ac6
1185491736@qq.com <1185491736@qq.com>
2019-09-13 21:48:01 +0800

    231

---
+++ 190颠倒二进制位.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    let res = 0
+    for (let i = 0; i < 32; i++) {
+        res <<= 1
+        res += n & 1
+        n >>= 1
+    }
+    return res >>> 0
+};
