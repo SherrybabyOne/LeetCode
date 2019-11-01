41dd9c71d8d4b041f5b006a7dabd4b422c6e23a5
1185491736@qq.com <1185491736@qq.com>
2019-11-01 23:31:42 +0800

    快速排序

--- sort/排序算法.js
+++ sort/排序算法.js
@@ -1,4 +1,5 @@
 // 冒泡排序
+// 时间复杂度: O(n^2)
 const bubbleSort = arr => {
   for(let i = arr.length - 1; i > 0; i--) {
     for(let j = 0; j < i; j ++) {
@@ -8,6 +9,7 @@ const bubbleSort = arr => {
   return arr;
 }
 // 选择排序
+// 时间复杂度: O(n^2)
 const selectSort1 = arr => {
   for(let i = 0; i < arr.length - 1; i++) {
     let min = arr[i];
@@ -32,3 +34,34 @@ const selectSort2 = arr => {
 console.log(bubbleSort([2,6,1,8,5]))
 console.log(selectSort1([2,6,1,8,5]))
 console.log(selectSort2([2,6,1,8,5]))
+
+// 快速排序
+function quickSort(arr) {
+  quick(arr, 0, arr.length - 1);
+  return arr;
+}
+function quick(arr, start, end) {
+  if(start === end) return;
+  const index = partition(arr, start, end);
+  if(index > start) quick(arr, start, index - 1);
+  if(index < end) quick(arr, index + 1, end);
+}
+// 该方法的实现目的是: 在数组arr中从start到end中选择中间一项为主元，比主元小的排到主元左边，比主元大的排到主元右边，返回主元的索引
+function partition(arr, start, end) {
+  let index = Math.floor((start + end) / 2);
+  [arr[index], arr[end]] = [arr[end], arr[index]];
+
+  let small = start - 1;
+  for(index = start; index < end; index++) {
+    if(arr[index] < arr[end]) {
+      small++;
+      if(small !== index) {
+        [arr[small], arr[index]] = [arr[index], arr[small]];
+      }
+    }
+  }
+  small++;
+  [arr[small], arr[end]] = [arr[end], arr[small]];
+  return small;
+}
+console.log(quickSort([2,6,1,8,5]))
