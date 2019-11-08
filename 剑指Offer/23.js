68ab956473ab1ce38385d29b708aa2a0b8d57384
1185491736@qq.com <1185491736@qq.com>
2019-11-08 17:56:55 +0800

    23 链表中环的入口节点

--- 剑指Offer/23.js
+++ 剑指Offer/23.js
@@ -14,5 +14,39 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-      
-};
+  const meetingNode = findMeetingNode(head);
+  if(!meetingNode) return null;
+  let nodesloop = 1;
+  let pNext = meetingNode;
+  while(pNext.next != meetingNode) {
+      nodesloop++;
+      pNext = pNext.next;
+  }
+  let pNode1 = head;
+  let pNode2 = head;
+  for(let i = 0; i < nodesloop; i++) {
+      pNode1 = pNode1.next;
+  }
+  while(pNode1 !== pNode2) {
+      pNode1 = pNode1.next;
+      pNode2 = pNode2.next;
+  }
+  return pNode1;
+}
+function findMeetingNode(head) {
+  if(!head) return null;
+  let pSlow = head.next;
+  if(!pSlow) return null;
+  let pFast = pSlow.next;
+  while(pSlow && pFast) {
+      if(pSlow == pFast) {
+          return pSlow;
+      }
+      pSlow = pSlow.next;
+      pFast = pFast.next;
+      if(pFast) {
+          pFast = pFast.next;
+      }
+  }
+  return null;
+}
