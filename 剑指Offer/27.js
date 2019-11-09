9eaccfe77022075770cdfe5feffd3733751b96d4
1185491736@qq.com <1185491736@qq.com>
2019-11-09 09:12:15 +0800

    27 二叉树的镜像

---
+++ 剑指Offer/27.js
@@ -0,0 +1,13 @@
+// 面试题27: 二叉树的镜像
+// LeetCode原题102对称二叉树
+const isSymmetric = (root) => {
+  if(!root) return;
+  if(!root.left && !root.right) return;
+  if(root.left && root.right) {
+     const temp = root.right;
+     root.right = root.left;
+     root.left = temp
+  }
+  if(root.left) isSymmetric(root.left);
+  if(root.right) isSymmetric(root.right);
+}
