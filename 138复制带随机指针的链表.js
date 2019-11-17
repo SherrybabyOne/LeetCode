f60a808505b9ed5abd9797b9fd4503160748cb47
1185491736@qq.com <1185491736@qq.com>
2019-11-17 23:16:40 +0800

    138

---
+++ 138复制带随机指针的链表.js
@@ -0,0 +1,37 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,next,random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+  if(!head) return null;
+  const hashMap = new Map();
+  const root = new Node(head.val);
+  let newNode1 = root;
+  let newNode2 = root;
+  let node1 = head.next;
+  let node2 = head;
+  
+  hashMap.set(head, root);
+  hashMap.set(null, null);
+  while(node1) {
+    const next = new Node(node1.val);
+    newNode1.next = next;
+    newNode1 = next;
+    hashMap.set(node1, newNode1);
+    node1 = node1.next;
+  }
+  while(node2) {
+    newNode2.random = hashMap.get(node2.random)
+    newNode2 = newNode2.next;
+    node2 = node2.next;
+  }
+  return root;
+};
