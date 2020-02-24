031132ce1248bf76f51ef4fd88c00145a0829600
1185491736@qq.com <1185491736@qq.com>
2020-02-24 22:23:12 +0800

    fix: 数据结构-栈修改

--- 数据结构/队列/单链队列.js
+++ 数据结构/队列/单链队列.js
@@ -1,21 +1,25 @@
 // 先进先出的数据结构
 // 浏览器/NodeJs中关于宏任务和微任务
 class Queue {
-    constructor(...items) {
-        this.reverse = false;
-        this.queue = [...items];
+    constructor() {
+        this.queue = [];
     }
 
     enqueue(...items) {
-        this.reverse ? this.queue.push(...items) : this.queue.unshift(...items);
+        return this.queue.unshift(...items);
     }
-
     dequeue() {
-        this.reverse ? this.queue.shift() : this.queue.pop();
+        return this.isEmpty() ? undefined : this.queue.shift();
+    }
+    isEmpty() {
+        return this.queue.size === 0;
+    }
+    size() {
+        return this.queue.length;
     }
 }
 
-const queue = new Queue(1, 2);
+const queue = new Queue();
 queue.enqueue(3, 4);
 queue.dequeue();
 console.log(queue);
