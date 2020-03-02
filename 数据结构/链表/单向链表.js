5c7df244c15fc5568fb5700b1042043ed42f0acb
1185491736@qq.com <1185491736@qq.com>
2020-03-02 23:20:29 +0800

    feat: 增加单向链表

--- 数据结构/链表/单向链表.js
+++ 数据结构/链表/单向链表.js
@@ -15,39 +15,9 @@ class LinkList {
     this.equalsFn = equalsFn;
   }
 
-  push(element) {
-    const node = new Node(element);
-    if (this.head === undefined) {
-      this.head = node;
-    } else {
-      let current = this.head;
-      while (current.next != undefined) {
-        current = current.next;
-      }
-      current.next = node;
-    }
-    this.count++;
-  }
-  remove(index) {
-    if (index >= 0 && index < this.count) {
-      let current = this.head;
-      if (index === 0) {
-        this.head = current.next;
-      } else {
-        let previous;
-        for (let i = 0; i < index; i++) {
-          previous = current;
-          current = current.next;
-        }
-        previous.next = current.next;
-      }
-      this.count--;
-      return current.element;
-    }
-    return undefined;
-  }
+  // 获取第index个元素（从0开始）
   getElementAt(index) {
-    if (index >= 0 && index < this.count) {
+    if (index >= 0 && index <= this.count) {
       let current = this.head;
       for(let i = 0; i < index && current != null; i++) {
         current = current.next;
@@ -56,71 +26,90 @@ class LinkList {
     }
     return undefined;
   }
+  // 向链表末尾添加元素
+  push(element) {
+    const node = new Node(element);
+    if (this.head === undefined) {
+      this.head = node;
+    } else {
+      const current = this.getElementAt(this.count - 1);
+      current.next = node;
+    }
+    this.count++;
+  }
+  // 移除第index个元素（从0元素）
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+  // 在任意位置插入元素
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        previous.next = node;
+        node.next = current;
+      }
+      this.count ++;
+      return true;
+    }
+    return false;
+  }
+  // 返回一个元素的位置
+  indexOf(element) {
+    let current = this.head;
+    for(let i = 0; i < this.count && current != undefined; i++) {
+      if (current.element === element) {
+        return i;
+      }
+      current = current.next;
+    }
+    return -1;
+  }
+  // 移除指定元素
+  remove(element) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+  // 链表长度
+  size() {
+    return this.count;
+  }
+  // 链表是否为空
+  isEmpty() {
+    return this.size() === 0;
+  }
+  // 返回链表头部
+  getHead() {
+    return this.head;
+  }
+  // 返回链表字符串
+  toString() {
+    if (this.head == undefined) {
+      return '';
+    }
+    let objString = `${this.head}`;
+    let current = this.head.next;
+    for(let i = 1; i < this.size() && current.next != undefined; i++) {
+      objString = `${objString}, ${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
 }
-
-const list = new LinkList();
-list.push('a');
-list.push('b');
-console.log(list);
-// class LinkList {
-//   constructor() {
-//     // 链表长度
-//     this.size = 0
-//     // 虚拟头部
-//     this.dummyNode = new Node(null, null)
-//   }
-//   find(header, index, currentIndex) {
-//     if (index === currentIndex) return header
-//     return this.find(header.next, index, currentIndex + 1)
-//   }
-//   addNode(v, index) {
-//     this.checkIndex(index)
-//     // 当往链表末尾插入时，prev.next 为空
-//     // 其他情况时，因为要插入节点，所以插入的节点
-//     // 的 next 应该是 prev.next
-//     // 然后设置 prev.next 为插入的节点
-//     let prev = this.find(this.dummyNode, index, 0)
-//     prev.next = new Node(v, prev.next)
-//     this.size++
-//     return prev.next
-//   }
-//   insertNode(v, index) {
-//     return this.addNode(v, index)
-//   }
-//   addToFirst(v) {
-//     return this.addNode(v, 0)
-//   }
-//   addToLast(v) {
-//     return this.addNode(v, this.size)
-//   }
-//   removeNode(index, isLast) {
-//     this.checkIndex(index)
-//     index = isLast ? index - 1 : index
-//     let prev = this.find(this.dummyNode, index, 0)
-//     let node = prev.next
-//     prev.next = node.next
-//     node.next = null
-//     this.size--
-//     return node
-//   }
-//   removeFirstNode() {
-//     return this.removeNode(0)
-//   }
-//   removeLastNode() {
-//     return this.removeNode(this.size, true)
-//   }
-//   checkIndex(index) {
-//     if (index < 0 || index > this.size) throw Error('Index error')
-//   }
-//   getNode(index) {
-//     this.checkIndex(index)
-//     if (this.isEmpty()) return
-//     return this.find(this.dummyNode, index, 0).next
-//   }
-//   isEmpty() {
-//     return this.size === 0
-//   }
-//   getSize() {
-//     return this.size
-//   }
-// }
