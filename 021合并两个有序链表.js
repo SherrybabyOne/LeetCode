e5e30412e0b25dfcbb168956663bd2857ef4d8e4
1185491736@qq.com <1185491736@qq.com>
2020-03-07 20:48:13 +0800

    021

--- 021合并两个有序链表.js
+++ 021合并两个有序链表.js
@@ -11,17 +11,13 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    if (l1 == null) {
-        return l2;
-    }
-    if (l2 == null) {
-        return l1;
-    }
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
     if (l1.val < l2.val) {
-        l1.next = mergeTwoLists(l1.next,l2);
+        l1.next = mergeTwoLists(l1.next, l2);
         return l1;
     } else {
-        l2.next = mergeTwoLists(l1,l2.next);
+        l2.next = mergeTwoLists(l1, l2.next);
         return l2;
     }
 };
