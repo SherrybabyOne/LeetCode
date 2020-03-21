4afdb358336ae8bec855483b53d2f1700eff4c43
1185491736@qq.com <1185491736@qq.com>
2020-03-22 00:02:42 +0800

    036

--- 036有效的数独.js
+++ 036有效的数独.js
@@ -13,9 +13,17 @@ var isValidSudoku = function(board) {
   }
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      if (board[i][j] !== '') {
-        const box_index = 
+      const item = board[i][j];
+      if (item !== '.') {
+        const box_index = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+        if (rows[i].has(item) || column[j].has(item) || boxes[box_index].has(item)) {
+          return false;
+        }
+        rows[i].set(item, 1);
+        column[j].set(item, 1);
+        boxes[box_index].set(item, 1);
       }
     }
   }
+  return true;
 };
