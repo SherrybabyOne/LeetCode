a47f681bff223e24e19705da741c6a79d6526048
1185491736@qq.com <1185491736@qq.com>
2020-03-31 23:44:23 +0800

    104

--- 104二叉树的最大深度.js
+++ 104二叉树的最大深度.js
@@ -10,6 +10,6 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (root === null) return 0;
-    return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+    if (!root) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
