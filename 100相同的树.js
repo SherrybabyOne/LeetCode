eb1600b4779a1f40a76f6221a40e3eb1e38fc6e9
1185491736@qq.com <1185491736@qq.com>
2020-04-01 23:58:40 +0800

    100

--- 100相同的树.js
+++ 100相同的树.js
@@ -10,35 +10,7 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-// 递归 先序遍历
-var isSameTree = function(p, q) {
-  if(!p && !q) return true;
-  if(!p || !q) return false;
-  if(p.val !== q.val) return false;
-  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-};
-
-// 层次遍历 广度优先搜索
-var isSameTree = function(p, q) {
-  const queue = [ [p, q] ];
-  while(queue.length) {
-    const [a, b] = queue.shift();
-    if(!booleanTwo(a, b)) return false;
-    if(a) {
-      queue.push([a.left, b.left]);
-      queue.push([a.right, b.right]);
-    }
-  }
-  return true;
-}
-function booleanTwo(a, b) {
-  if(!a && !b) return true;
-  if(!a || !b) return false;
-  if(a.val !== b.val) return false;
-  return true;
-}
-
-// 深度优先遍历
+// 递归 深度优先遍历
 var isSameTree = function(p, q) {
   if (!p && !q) return true;
   if (!p || !q) return false;
@@ -46,12 +18,12 @@ var isSameTree = function(p, q) {
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
 
-// 广度优先遍历
+// 循环 广度优先遍历
 var isSameTree = function(p, q) {
   const queue = [[p, q]];
   while(queue.length) {
     const [a, b] = queue.shift();
-    if (!a && !b) break;
+    if (!a && !b) continue;
     if (!a || !b) return false;
     if (a.val !== b.val) return false;
     queue.push([a.left, b.left]);
