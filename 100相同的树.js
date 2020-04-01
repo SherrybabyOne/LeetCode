965baead85bf29d28412cb81f4e06d00c2fbdb5c
1185491736@qq.com <1185491736@qq.com>
2020-04-01 23:57:28 +0800

    100

--- 100相同的树.js
+++ 100相同的树.js
@@ -37,3 +37,25 @@ function booleanTwo(a, b) {
   if(a.val !== b.val) return false;
   return true;
 }
+
+// 深度优先遍历
+var isSameTree = function(p, q) {
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// 广度优先遍历
+var isSameTree = function(p, q) {
+  const queue = [[p, q]];
+  while(queue.length) {
+    const [a, b] = queue.shift();
+    if (!a && !b) break;
+    if (!a || !b) return false;
+    if (a.val !== b.val) return false;
+    queue.push([a.left, b.left]);
+    queue.push([a.right, b.right]);
+  }
+  return true;
+};
