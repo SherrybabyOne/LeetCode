82434dc587a6e378284562256dd79c35399ef897
aihao <1185491736@qq.com>
2020-04-04 22:48:18 +0800

    215

--- 215数组中的第K个最大元素.js
+++ 215数组中的第K个最大元素.js
@@ -4,17 +4,6 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    return nums.sort((a,b)=>b-a)[k-1]
+	nums.sort((a, b) => b- a);
+	return nums[k - 1];
 };
-
-
-
-// var findKthLargest = function(nums, k) {
-//     let leng = nums.length;
-//     for(let i = leng-1; i>(leng-k-1); i--) {
-//         for(let j = 0; j<i; j++) {
-//             if(nums[j] > nums[j+1]) [nums[j],nums[j+1]] = [nums[j+1],nums[j]];
-//         }
-//     }
-//     return nums[leng-k]
-// };
