86732230a04145246414a22d926bea01a1c8950b
aihao <1185491736@qq.com>
2020-04-04 22:06:32 +0800

    001

--- 001两数之和.js
+++ 001两数之和.js
@@ -4,25 +4,18 @@
  * @param {number} target
  * @return {number[]}
  */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function(nums, target) {
-    let res = {};
-    for(let i = 0; i<nums.length; i++){
-        if(res[target-nums[i]] != undefined){
-            return [i,res[target-nums[i]]]
-        }
-        res[nums[i]] = i;
-    }
-};
-
-
-var twoSum = function(nums, target) {
-    let map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            return [i, map.get(nums[i])]
-        } else {
-            map.set(target - nums[i], i)
-        }
-    }
-    return []
+	const map = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		if (!map.has(nums[i])) {
+			map.set(target - nums[i], i);
+		} else {
+			return [map.get(nums[i]), i];
+		}
+	}
 };
