1d25fa5caaf8ecf7790927d21fb3042b94376249
aihao <1185491736@qq.com>
2020-04-04 21:14:16 +0800

    feat: 树的遍历

--- 数据结构/树/遍历.js
+++ 数据结构/树/遍历.js
@@ -1,35 +1,32 @@
 // 递归方法
 // 深度遍历
 
-
 // 先序遍历可用于打印树的结构
-var preorderTraversal = function (root, array=[]) {
-    if(root) {
-        array.push(root.val)
-        preorderTraversal(root.left, array);
-        preorderTraversal(root.right, array)
-    }
-    return array;
+const preorderTraversal = function (root, array=[]) {
+	if(root) {
+		array.push(root.val)
+		preorderTraversal(root.left, array);
+		preorderTraversal(root.right, array)
+	}
+	return array;
 }
-
 
 // 中序遍历可用于排序
-var inorderTraversal = function (root, array=[]) {
-    if (root) {
-        inorderTraversal(root.left, array);
-        array.push(root.val);
-        inorderTraversal(root.right, array);
-    }
-    return array;
+const inorderTraversal = function (root, array=[]) {
+	if (root) {
+		inorderTraversal(root.left, array);
+		array.push(root.val);
+		inorderTraversal(root.right, array);
+	}
+	return array;
 }
 
-
 // 后序遍历可用于线操作子节点
-var postorderTraversal = function (root, array=[]) {
-    if (root) {
-        postorderTraversal(root.left, array);
-        postorderTraversal(root.right, array);
-        array.push(root.val);
-    }
-    return array;
+const postorderTraversal = function (root, array=[]) {
+	if (root) {
+		postorderTraversal(root.left, array);
+		postorderTraversal(root.right, array);
+		array.push(root.val);
+	}
+	return array;
 }
