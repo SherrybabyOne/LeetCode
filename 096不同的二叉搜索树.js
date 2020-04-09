662505b0270b2cb51a7446497f8b5b101be22004
aihao <1185491736@qq.com>
2020-04-10 00:59:14 +0800

    096

--- 096不同的二叉搜索树.js
+++ 096不同的二叉搜索树.js
@@ -4,12 +4,12 @@
  */
 var numTrees = function(n) {
   const G = [];
-  G[0] = 0;
+  G[0] = 1;
   G[1] = 1;
   for (let i = 2; i <= n; i++) {
     G[i] = 0;
-    for (let j = 1; j < i; j++) {
-      G[n] += G[j - 1] * G[i - j];
+    for (let j = 1; j <= i; j++) {
+      G[i] += G[j - 1] * G[i - j];
     }
   }
   return G[n];
