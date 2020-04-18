b1f26fe5b3d99e29c20765274953c03abfd90aa5
aihao <1185491736@qq.com>
2020-04-19 00:19:28 +0800

    236

--- 236二叉树的最近公共祖先.js
+++ 236二叉树的最近公共祖先.js
@@ -11,6 +11,37 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+// 非递归写法
+var lowestCommonAncestor = function(root, p, q) {
+  if (root == null || root == p || root == q) return root;
+  const map = new Map();
+  const set = new Set();
+  const queue = [];
+  queue.push(root);
+  while (queue.length) {
+    let size = queue.length;
+    while (size --) {
+      const item = queue.shift();
+      if (item.left) {
+        queue.push(item.left);
+        map.set(item.left, item);
+      }
+      if (item.right) {
+        queue.push(item.right);
+        map.set(item.right, item);
+      }
+    }
+  }
+  while (p) {
+    set.add(p);
+    p = map.get(p);
+  }
+  while (q) {
+    if (set.has(q)) return q;
+    q = map.get(q);
+  }
+};
+// 递归写法
 var lowestCommonAncestor = function(root, p, q) {
   if (root == null || root == p || root == q) return root;
   const left = lowestCommonAncestor(root.left, p, q);
