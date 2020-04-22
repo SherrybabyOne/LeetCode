75bb2a0317f949e6c5c28b3f61317f99c1d72e93
aihao <1185491736@qq.com>
2020-04-23 00:05:08 +0800

    098

--- 098验证二叉搜索树.js
+++ 098验证二叉搜索树.js
@@ -9,9 +9,31 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-// 中序遍历
+// 中序遍历-非递归
 var isValidBST = function(root) {
-  
+  if (!root) return true;
+  const stack = [];
+  const res = [];
+  let cur = root;
+  while (stack.length || cur) {
+    while (cur) {
+      stack.push(cur);
+      cur = cur.left;
+    }
+    cur = stack.pop();
+    res.push(cur.val);
+    cur = cur.right;
+  }
+  return res.every((item, index) => {
+    if (index !== res.length - 1) {
+      if (item >= res[index + 1]) return false;
+    }
+    return true;
+  })
+};
+// 中序遍历-递归
+var isValidBST = function(root) {
+
 };
 // 递归DFS
 var isValidBST = function(root, lowser = null, upper = null) {
@@ -19,4 +41,8 @@ var isValidBST = function(root, lowser = null, upper = null) {
   if (lowser !== null && lowser >= root.val) return false;
   if (upper !== null && upper <= root.val) return false;
   return isValidBST(root.left, lowser, root.val) && isValidBST(root.right, root.val, upper);
+};
+// 非递归-DFS
+var isValidBST = function(root) {
+
 };
