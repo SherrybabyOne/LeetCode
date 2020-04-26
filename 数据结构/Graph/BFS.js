72e9e39cddac81f6ebfe135265c0239e609a7cd6
aihao <1185491736@qq.com>
2020-04-26 23:46:15 +0800

    BFS

---
+++ 数据结构/Graph/BFS.js
@@ -0,0 +1,15 @@
+// BFS：广度优先遍历
+// 先宽后深地访问顶点
+const breadthFirstSearch = node => {
+  const nodeList = [];
+  const queue = [node];
+  while (queue.length) {
+    const item = queue.shift();
+    nodeList.push(item);
+    const children = item.children;
+    for (let i = 0; i < children.length; i++) {
+      queue.push(children[i]);
+    }
+  }
+  return nodeList;
+}
