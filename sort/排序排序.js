eac6b7a8e818d2d3a832676ac237afc8a00d422b
aihao <1185491736@qq.com>
2020-04-28 00:00:35 +0800

    feat: 选择排序

--- sort/排序排序.js
+++ sort/排序排序.js
@@ -1,31 +1,3 @@
-// 选择排序：原址比较排序算法。大致思路是找到数据结构中的最小值并将其放在第一位，
-// 接着找到第二小值并将其放在第二位，以此类推。
-// 时间复杂度: O(n^2)
-// const selectSort = arr => {
-//   for(let i = 0; i < arr.length - 1; i++) {
-//     let minIndex = i;
-//     for(let j = i + 1; j < arr.length; j++) {
-//       minIndex = arr[j] < arr[minIndex] ? j : minIndex;
-//     }
-//     swap(arr, i, minIndex);
-//   }
-//   return arr;
-// }
-// console.log(selectSort([2,6,1,8,5]))
-const selectionSort = arr => {
-  const len = arr.length;
-  for (let i = 0; i < len; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < len; j++) {
-      minIndex = arr[j] < arr[minIndex] ? j : minIndex;
-    }
-    if (minIndex !== i) [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
-  }
-  return arr;
-}
-console.log(selectSort([2,6,1,8,5]))
-
-
 // 三、插入排序
 // 时间复杂读: O(n * n)
 const insertSort = (arr) => {
--- sort/选择排序.js
+++ sort/选择排序.js
@@ -1,12 +1,17 @@
-var selectSort = function(arr) {
-    for(let i = 0; i<arr.length; i++) {
-        let min = arr[i];
-        for(let j = i+1; j<arr.length; j++) {
-            if(min > arr[j]) [arr[j],min] = [min,arr[j]]
-        }
-        arr[i] = min;
+// 选择排序：原址比较排序算法。大致思路是找到数据结构中的最小值并将其放在第一位，
+// 接着找到第二小值并将其放在第二位，以此类推。
+// 时间复杂度: O(n^2)
+const selectionSort = arr => {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      minIndex = arr[j] < arr[minIndex] ? j : minIndex;
     }
-    return arr;
-}
+    if (minIndex !== i) [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+  }
+  return arr;
+};
 
-console.log(selectSort([2,6,1,8,5]))
+console.log(selectionSort([2,6,1,8,5]));
+console.log(selectionSort([5,4,3,2,1]));
