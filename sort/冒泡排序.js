b4e084a06172d795fd99a32bfe0108b5f494d620
aihao <1185491736@qq.com>
2020-04-27 23:48:35 +0800

    feat: 冒泡排序

---
+++ sort/冒泡排序.js
@@ -0,0 +1,15 @@
+// 冒泡排序
+// 时间复杂度：O(n * n)
+const modifiedBubbleSort = arr => {
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+  return arr;
+}
+
+const arr = [5, 4, 3, 2, 1, 0];
+modifiedBubbleSort(arr);
+console.log(arr);
