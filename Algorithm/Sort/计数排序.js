81c7d7ae4e9ad336b4a1c734de416720d422bbad
aihao <1185491736@qq.com>
2020-05-06 18:20:55 +0800

    feat: 计数排序

---
+++ Algorithm/Sort/计数排序.js
@@ -0,0 +1,35 @@
+// 计数排序
+// 分布式排序，是一个正整数排序算法
+// 时间复杂度：O(n + k)
+// 需要更多的内存来存放临时数组
+const countingSort = arr => {
+  if (arr.length < 2) return arr;
+  const maxValue = findMaxValue(arr);
+  const counts = new Array(maxValue + 1);
+
+  arr.forEach(element => {
+    if (!counts[element]) counts[element] = 0;
+    counts[element]++; 
+  })
+
+  let sortedIndex = 0;
+  counts.forEach((count, i) => {
+    if (count > 0) arr[sortedIndex++] = i;
+  })
+  return arr;
+}
+
+const findMaxValue = arr => {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(
+  countingSort([5, 4, 3, 2, 1]),
+  countingSort([9, 6, 2]),
+)
