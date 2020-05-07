eb0e26b20c65fe4e2c1e73fe6c250361239f3c0c
aihao <1185491736@qq.com>
2020-05-07 22:10:14 +0800

    feat: 二分搜索

--- Algorithm/Search/二分搜索.js
+++ Algorithm/Search/二分搜索.js
@@ -1,12 +1,12 @@
 // 二分搜索
 // 要求是已经排序好的数组
-const binarySort = (arr, value) => {
+const binarySearch = (arr, value) => {
   const sortedArr = arr.sort((a, b) => a - b);
   let low = 0;
-  let high = arr.length - 1;
+  let high = sortedArr.length - 1;
   while (low <= high) {
     const mid = Math.floor((low + high) / 2);
-    const pivot = arr[mid];
+    const pivot = sortedArr[mid];
     if (pivot < value) {
       low = mid + 1;
     } else if (pivot > value) {
@@ -18,7 +18,24 @@ const binarySort = (arr, value) => {
   return -1;
 }
 
-console.log(
-  // binarySort([8, 7, 6, 5, 4, 3, 2, 1], 7),
-  binarySort([0, 1], 1),
-)
+// 分治算法
+const binarySearch = (arr, value) => {
+  const sortedArr = arr.sort((a, b) => a - b);
+  const low = 0;
+  const high = sortedArr.length - 1;
+  return binarySearchRecursive(arr, value, low, high);
+}
+const binarySearchRecursive = (arr, value, low, high) => {
+  if (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const pivot = arr[mid];
+    if (pivot < value) {
+      return binarySearchRecursive(arr, value, mid + 1, high);
+    } else if (pivot > value) {
+      return binarySearchRecursive(arr, value, low, mid - 1);
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
