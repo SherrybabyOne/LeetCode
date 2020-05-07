b6d0898e0f2fc571bd99d8d2c3f0a3df09bef3cc
aihao <1185491736@qq.com>
2020-05-08 00:10:59 +0800

    169

--- 169多数元素.js
+++ 169多数元素.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 哈希表写法
 var majorityElement = function(nums) {
   const map = new Map();
   const min = Math.floor(nums.length / 2);
@@ -16,3 +17,25 @@ var majorityElement = function(nums) {
   }
   return false;
 };
+
+// 分治写法
+var majorityElement = function(nums) {
+  return majorityElementRec(nums, 0, nums.length - 1);
+};
+var majorityElementRec = function(nums, lo, hi) {
+  if (lo === hi) return nums[lo];
+  const mid = Math.floor((hi + lo) / 2);
+  const left = majorityElementRec(nums, lo, mid);
+  const right = majorityElementRec(nums, mid + 1, hi);
+  if (left === right) return left;
+  const leftCount = countInRange(nums, left, lo, hi);
+  const rightCount = countInRange(nums, right, lo, hi);
+  return leftCount > rightCount ? left : right;
+}
+var countInRange = function(nums, num, lo, hi) {
+  let count = 0;
+  for (let i = lo; i <= hi; i++) {
+    if (nums[i] === num) count++;
+  }
+  return count;
+}
