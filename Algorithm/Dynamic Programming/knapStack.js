8ecc55026ba78934cbde4c14ca6b2dcea27502c2
aihao <1185491736@qq.com>
2020-05-17 00:24:55 +0800

    feat: 背包0-1问题

--- Algorithm/Dynamic Programming/knapStack.js
+++ Algorithm/Dynamic Programming/knapStack.js
@@ -1,4 +1,6 @@
 // 0-1背包问题
+// 给定一个固定大小、能够携重量W的背包，以及一组有价值和重量的物品
+// 找一个最佳解决方案，使得装入背包的物品不超过W，且总价值最大
 const knapStack = (capacity, weights, values, n) => {
   const kS = [];
   for (let i = 0; i <= n; i++) {
@@ -17,7 +19,22 @@ const knapStack = (capacity, weights, values, n) => {
       }
     }
   }
+  findValues(n, capacity, kS, weights, values);
   return kS[n][capacity];
+}
+
+const findValues = (n, capacity, kS, weights, values) => {
+  let k = capacity;
+  console.log('构成解的物品:')
+  while (n > 0 && k > 0) {
+    if (kS[n][k] !== kS[n - 1][k]) {
+      console.log(`物品${n}是可解的一部分 w,v: ${weights[n - 1]}, ${values[n - 1]}}`);
+      n--;
+      k -= weights[n - 1];
+    } else {
+      n--;
+    }
+  }
 }
 
 const values = [3, 4, 5],
@@ -26,4 +43,4 @@ const values = [3, 4, 5],
       n = values.length;
 console.log(
   knapStack(capacity, weights, values, n),
-)
+);
