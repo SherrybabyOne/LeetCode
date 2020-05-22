4ebcd91f5cf0e6e5baad7f9522e4b2a7259bb180
aihao <1185491736@qq.com>
2020-05-22 09:32:13 +0800

    feat: 173

---
+++ LeetCode/173二叉搜索树迭代器.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+// const inorderTraversal = (node, arr) => {
+//   if (node) {
+//     inorderTraversal(node.left, arr);
+//     arr.push(node.val);
+//     inorderTraversal(node.right, arr);
+//   }
+// }
+const inorderTraversal = root => {
+  if (!root) return [];
+  const stack = [];
+  const res = [];
+  let current = root;
+  while (stack.length || current) {
+    while (current) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop();
+    res.push(current.val);
+    current = current.right;
+  }
+  return res;
+}
+var BSTIterator = function(root) {
+  const arr = inorderTraversal(root);
+  this.arr = arr;
+  this.count = 0;
+  this.len = arr.length;
+};
+
+/**
+ * @return the next smallest number
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+  if (this.count < this.len) {
+    return this.arr[this.count++];
+  }
+};
+
+/**
+ * @return whether we have a next smallest number
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+  return this.count < this.len;
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
