cb459656e901e6a84267148369e1c72c56824579
aihao <1185491736@qq.com>
2020-06-21 23:48:34 +0800

    fix: 快排修改

--- Algorithm/Sort/快速排序.js
+++ Algorithm/Sort/快速排序.js
@@ -4,10 +4,11 @@
 // 1. 选择一个值为主元（也就是数组中间的那个值）
 // 2. 创建两个指针分别指向第一个和最后一个，移动左指针直到找到比主元大的值，
 // 移动右指针直到找到比主元小的值，然后交换它们。直到左指针超过了右指针。
-// 这个过程使得比主元小的都排到主元之前，比主元大的都排到主元之后。称为划分（partition）操作。
+// 这个过程使得比主元小的都排到最终的左指针之前，大于等于主元的都排到右指针之后。称为划分（partition）操作。
 // 3. 对划分后的两个自数组重复2的步骤，直至整个数组完全排序
 function quickSort(arr) {
-  return quick(arr, 0, arr.length - 1);
+  quick(arr, 0, arr.length - 1);
+  return arr;
 }
 function quick(arr, start, end) {
   if (arr.length > 1) {
@@ -19,14 +20,13 @@ function quick(arr, start, end) {
       quick(arr, index, end);
     }
   }
-  return arr;
 }
 // 该方法的实现目的是: 在数组arr中从start到end中选择中间一项为主元，比主元小的排到主元左边，比主元大的排到主元右边
 function partition(arr, start, end) {
   const pivot = arr[Math.floor((start + end) / 2)];
   let i = start;
   let j = end;
-  while (i <= j) {
+  while (i < j) {
     while (arr[i] < pivot) {
       i++;
     }
---
+++ 二刷剑指Offer/JZ28.js
@@ -0,0 +1,44 @@
+function MoreThanHalfNum_Solution(numbers) {
+  sort(numbers);
+  const cnt = numbers[Math.floor(numbers.length / 2)];
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (cnt === numbers[i]) count++;
+  }
+  return count > numbers.length / 2 ? cnt : 0;
+}
+
+function sort(arr) {
+  quickSort(arr, 0, arr.length - 1);
+  return arr;
+}
+function quickSort(arr, start, end) {
+  if (arr.length > 1) {
+    const index = partition(arr, start, end);
+    if (start < index - 1) {
+      quickSort(arr, start, index - 1);
+    }
+    if (index < end) {
+      quickSort(arr, index, end);
+    }
+  }
+}
+function partition(arr, start, end) {
+  const pivot = arr[Math.floor((start + end) / 2)];
+  let i = start;
+  let j = end;
+  while (i < j) {
+    while (arr[i] < pivot) {
+      i++;
+    }
+    while (arr[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+      j--;
+    }
+  }
+  return i;
+}
