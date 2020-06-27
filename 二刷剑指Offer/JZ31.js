81c3f65b80881b9485117810b55a4b1ef217db08
aihao <1185491736@qq.com>
2020-06-27 11:24:10 +0800

    feat: 31

--- 二刷剑指Offer/JZ31.js
+++ 二刷剑指Offer/JZ31.js
@@ -10,3 +10,24 @@ function NumberOf1Between1AndN_Solution(n) {
   }
   return count;
 }
+
+function NumberOf1Between1AndN_Solution(n) {
+  let res = 0, low = 0;
+  let digit = 1;
+  let high = Math.floor(n / 10);
+  let cur = n % 10;
+  while (high !== 0 || cur !== 0) {
+    if (cur === 0) {
+      res += high * digit;
+    } else if (cur === 1) {
+      res += high * digit + low + 1;
+    } else {
+      res += (high + 1) * digit;
+    }
+    low += digit * cur;
+    cur = high % 10;
+    high = Math.floor(high / 10);
+    digit *= 10;
+  }
+  return res;
+}
