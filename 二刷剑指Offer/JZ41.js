af5c834a5a55895bd40cc1a40ee8f479921055de
aihao <1185491736@qq.com>
2020-07-05 11:25:51 +0800

    feat: 41

--- 二刷剑指Offer/JZ41.js
+++ 二刷剑指Offer/JZ41.js
@@ -1,21 +1,47 @@
-function FindContinuousSequence(sum)
-{
-    // write code here
-    let result = [],pre=1,next=2;
-    while(pre < next) {
-        const total = (pre+next)*(next-pre+1)/2;
-        if(sum == total) {
-            const list = [];
-            for(let i =pre;i<=next;i++) {
-                list.push(i)
-            }
-            result.push(list)
-            pre++
-        } else if(total < sum) {
-            next++
-        } else {
-            pre++
-        }
-    }
-    return result
+// 前缀和
+// O(n * n)
+function FindContinuousSequence(sum) {
+	const res = [];
+	let temp = 0;
+	for (let i = 1; i < sum / 2; i++) {
+		for (let j = i; j < sum; j++) {
+			temp += j;
+			if (temp === sum) {
+				const ans = [];
+				for (let k = i; k <= j; k++) {
+					ans.push(k);
+				}
+				res.push(ans);
+			} else if (temp > sum) {
+				temp = 0;
+				break;
+			}
+		}
+	}
+	return res;
+}
+// 滑动窗口
+// O(n)
+function FindContinuousSequence(sum) {
+	let l = r = 1;
+	let temp = 0;
+	const res = [];
+	while (l <= sum / 2) {
+		if (temp < sum) {
+			temp += r;
+			r++;
+		} else if (temp > sum) {
+			temp -= l;
+			l++;
+		} else {
+			const ans = [];
+			for (let i = l; i < r; i++) {
+				ans.push(i);
+			}
+			res.push(ans);
+			temp -= l;
+			l++;
+		}
+	}
+	return res;
 }
