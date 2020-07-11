8b697a588f24105c7b0fe3c3bf9a0eb7beac0217
aihao <1185491736@qq.com>
2020-07-11 23:51:44 +0800

    feat: 215

--- LeetCode/215数组中的第K个最大元素.js
+++ LeetCode/215数组中的第K个最大元素.js
@@ -4,6 +4,31 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-	nums.sort((a, b) => b- a);
-	return nums[k - 1];
+	if (!nums) return null;
+	// if (nums.length === 1 || nums.length === 2) return nums[k - 1];
+	const res = quickSelect(nums, 0, nums.length - 1, nums.length - k);
+	return res;
 };
+const quickSelect = (nums, start, end, index) => {
+	const i = partition(nums, start, end);
+	// console.log(nums, i);
+	if (i === index) {
+		return nums[index];
+	} else if (i > index) {
+		return quickSelect(nums, start, i - 1, index);
+	} else {
+		return quickSelect(nums, i + 1, end, index);
+	}
+}
+const partition = (nums, l, r) => {
+	const pivot = nums[l];
+	let j = l;
+	for (let i = l + 1; i <= r; i++) {
+		if (nums[i] < pivot) {
+			j++;
+			[nums[j], nums[i]] = [nums[i], nums[j]];
+		}
+	}
+	[nums[j], nums[l]] = [nums[l], nums[j]];
+	return j;
+}
