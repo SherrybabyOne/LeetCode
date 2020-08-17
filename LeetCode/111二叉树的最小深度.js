1c80655c6d7832a080b0113fa780719fe76aee21
aihao <1185491736@qq.com>
2020-08-17 12:16:07 +0800

    feat: 111

--- LeetCode/111二叉树的最小深度.js
+++ LeetCode/111二叉树的最小深度.js
@@ -9,10 +9,28 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// 递归版本
 var minDepth = function(root) {
-    if (root === null) return 0;
-    let left = minDepth(root.left);
-    let right = minDepth(root.right);
-    if (left === 0 || right === 0) return left + right + 1;
-    return Math.min(left, right) + 1;
+	if (!root) return 0;
+	const left = minDepth(root.left);
+	const right = minDepth(root.right);
+	if (left === 0 || right === 0) return left + right + 1;
+	return Math.min(left, right) + 1;
+};
+// 非递归版本
+var minDepth = function(root) {
+	if (!root) return 0;
+	const queue = [root];
+	let ans = 1;
+	while (queue.length) {
+		const len = queue.length;
+		for (let i = 0; i < len; i++) {
+			const item = queue.shift();
+			if (!item.left && !item.right) return ans;
+			item.left && queue.push(item.left);
+			item.right && queue.push(item.right);
+		}
+		ans++;
+	}
+	return ans;
 };
