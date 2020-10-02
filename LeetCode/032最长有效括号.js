33ef43fd8884bf6da8eeda79d24a226e75c4f036
aihao <1185491736@qq.com>
2020-10-02 12:14:16 +0800

    feat: 032

--- LeetCode/032最长有效括号.js
+++ LeetCode/032最长有效括号.js
@@ -19,3 +19,21 @@ var longestValidParentheses = function(s) {
   }
   return max;
 };
+
+function longestValidParentheses(s) {
+  let max = 0;
+  const stack = [-1];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      stack.push(i);
+    } else {
+      stack.pop();
+      if (stack.length === 0) {
+        stack.push(i);
+      } else {
+        max = Math.max(max, i - stack[stack.length - 1]);
+      }
+    }
+  }
+  return max;
+}
