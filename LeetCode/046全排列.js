5395b8afb7a774028cbce34aca65a151c6c2d7d8
aihao <1185491736@qq.com>
2020-10-07 15:36:56 +0800

    feat: 046

---
+++ LeetCode/046全排列.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  const len = nums.length;
+  const output = [];
+  const res = [];
+  for (const item of nums) output.push(item);
+  backTrack(len, res, output, 0);
+  return res;
+};
+const backTrack = (len, res, output, cur) => {
+  if (cur === len) res.push([...output]);
+  for (let i = cur; i < len; i++) {
+    swap(output, cur, i);
+    backTrack(len, res, output, cur + 1);
+    swap(output, cur, i);
+  }
+}
+const swap = (arr, i, j) => {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+console.log(
+  permute([1, 2, 3, 4])
+)
