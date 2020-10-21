a213ef5ca503c69bbeea108766492718cf009c5c
aihao <1185491736@qq.com>
2020-10-21 23:44:14 +0800

    feat: 207

---
+++ LeetCode/207课程表.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+  let res = 0;
+  // 存储有向图
+  const edges = {};
+  // 存储每个节点的入度
+  const indeg = new Array(numCourses).fill(0);
+  for (let i = 0; i < prerequisites.length; i++) {
+    const courseIn = prerequisites[i][0];
+    const courseOut = prerequisites[i][1];
+    edges[courseOut]
+      ? edges[courseOut].push(courseIn)
+      :edges[courseOut] = [courseIn];
+    indeg[courseIn]++;
+  }
+  while (indeg.indexOf(0) !== -1) {
+    const index = indeg.indexOf(0);
+    indeg[index] = -1;
+    res++;
+    const u = edges[index];
+    if (u == undefined) continue;
+    for (const k of u) {
+      if (indeg[k] > 0) indeg[k]--;
+    }
+  }
+  return res === numCourses;
+};
