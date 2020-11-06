f126ac25024ef42b5009796075e2222aa39f2b09
aihao <1185491736@qq.com>
2020-11-06 20:30:17 +0800

    feat: 021

--- LeetCode/021合并两个有序链表.js
+++ LeetCode/021合并两个有序链表.js
@@ -11,13 +11,30 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    if (l1 == null) return l2;
-    if (l2 == null) return l1;
-    if (l1.val < l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else {
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2;
-    }
+	if (l1 == null) return l2;
+	if (l2 == null) return l1;
+	if (l1.val < l2.val) {
+		l1.next = mergeTwoLists(l1.next, l2);
+		return l1;
+	} else {
+		l2.next = mergeTwoLists(l1, l2.next);
+		return l2;
+	}
+};
+
+var mergeTwoLists = function(l1, l2) {
+	const preHead = new ListNode(-1);
+	let prev = preHead;
+	while (l1 != null && l2 != null) {
+		if (l1.val > l2.val) {
+			prev.next = l2;
+			l2 = l2.next;
+		} else {
+			prev.next = l1;
+			l1 = l1.next;
+		}
+		prev = prev.next;
+	}
+	prev.next = l1 === null ? l2 : l1;
+	return preHead.next;
 };
